5552f4ca232d6b47b1e3208567a9be632d3756f6
zxhoper <zxhoper@proton.me>
2023-07-05 10:29:26 +0800

    2023-07-05_10:29:26 commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module railway-go-http-001_2023-07-05
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", ExampleHandler)
+
+	log.Println("** Service Started on Port 8080 **")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func ExampleHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	io.WriteString(w, `{"status":"ok", "msg":"railway-go-http-001_2023-07-05"}`)
+}
